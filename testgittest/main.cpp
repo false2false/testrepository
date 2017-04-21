ab5aefaa12a38f4aed748eef46c0dc2d044e6aed
false2false <t81trrk@gmail.com>
2017-04-21 14:12:07 +0900

    Initial Commit

---
+++ testgittest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testgittest
+//
+//  Created by k14081kk on 2017/04/21.
+//  Copyright (c) 2017年 AIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
